5e0718ca767a9f7410df99a5da76cabb5e1a28c2
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-29 07:01:46 +0100

    add : mock data file

---
+++ src/data/mockIpdata.js
@@ -0,0 +1,18 @@
+export const mockIpData = [
+  {
+    name: "IP Address",
+    value: "192.212.12.90",
+  },
+  {
+    name: "Location",
+    value: "Brooklyn, NY 10001",
+  },
+  {
+    name: "Timezone",
+    value: "UTC -05:00",
+  },
+  {
+    name: "ISP",
+    value: "SpaceX Starlink",
+  },
+];
